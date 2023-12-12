f31b7dd080cb81700e341712d289b6dc80f47704
Sathishkumar-Ramachandran <sathish.spacy2001@gmail.com>
2023-12-12 21:32:10 +0530

    auth commit

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-    user: process.env.USER ||'prdn',
+    user: process.env.USER ||'postgres',
     host: process.env.HOST || localhost,
     database: process.env.DATABASE || 'users',
     password: process.env.PASSWORD || 'Sathish@123',
