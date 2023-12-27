1c04013a211eca445226cbd94d41e3d427b4ec95
Sathishkumar-Ramachandran <sathish.spacy2001@gmail.com>
2023-12-27 08:43:02 +0530

    Auth

--- src/logics/v1/smtpService.js
+++ src/logics/v1/smtpService.js
@@ -1,30 +1,112 @@
 const express = require('express');
 const signupLogic = require('../../logics/v1/signupLogic.js');
-const { get } = require('../../routes/v1/loginRoute');
 
 
-const smtpService = {
-    connection: () => {
 
-    },
+let otpverifyArray = [];
 
 
-    sendOTP: async () => {
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: "sathish.spacy2001@gmail.com", // your Gmail email address
+    pass: "Shivani@2001", // your Gmail password
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
 
+
+
+  
+const UserVerification = {
+  sendOtpToUser: async (email) => {
+    console.log(email);
+    let checkinArray = otpverifyArray.filter((x) => x.Email == email);
+    if (checkinArray.length > 0) {
+      let index = otpverifyArray.findIndex((item) => item.Email == email);
+      if (index !== -1) {
+        otpverifyArray.splice(index, 1);
+      }
     }
 
-} 
+    const otp = signupLogic.generateOTP(email);
+    const mailOptions = {
+      from: "sathish.spacy2001@gmail.com",
+      to: email,
+      subject: "Verification",
+      html: `  <div style="background-color: #f2f2f2; padding: 20px;">
+        <div style="background-color: #fff; padding: 20px; border-radius: 5px;">
+          <h1 style="color: #0051a0; font-size: 24px; font-weight: bold; margin-bottom: 20px;">Double Engine</h1>
+          <p style="font-size: 18px; margin-bottom: 20px;">Your verification code is:</p>
+          <h2 style="font-size: 48px; margin-bottom: 20px;">${otp}</h2>
+          <p style="font-size: 16px;">This code will expire in one minutes.</p>
+        </div>
+      </div>`,
+    };
+    try {
+      const response = await MailToSend(mailOptions);
+      if (response) {
+        otpverifyArray.push({
+          Email: email,
+          otp: otp,
+          time: new Date().getTime(),
+        });
 
-const mailBody = {
-    from: "sathish.spacy2001@gmail.com",
-    to: email,
-    subject: "Verification",
-    html: `  <div style="background-color: #f2f2f2; padding: 20px;">
-      <div style="background-color: #fff; padding: 20px; border-radius: 5px;">
-        <h1 style="color: #0051a0; font-size: 24px; font-weight: bold; margin-bottom: 20px;">Double Engine</h1>
-        <p style="font-size: 18px; margin-bottom: 20px;">Your verification code is:</p>
-        <h2 style="font-size: 48px; margin-bottom: 20px;">${otp}</h2>
-        <p style="font-size: 16px;">This code will expire in one minutes.</p>
-      </div>
-    </div>`,
-  };
+        return 1;
+      } else {
+        return undefined;
+      }
+    } catch (error) {
+      return undefined;
+    }
+  },
+  verifyOTP: async (email, otp) => {
+    try {
+      const info = otpverifyArray.filter((x) => x.Email == email);
+      if (info.length > 0) {
+        if (otp == info[0].otp) {
+          if (new Date().getTime() - info[0].time <= 60000) {
+            let index = otpverifyArray.findIndex((item) => item.Email == email);
+            if (index !== -1) {
+              otpverifyArray.splice(index, 1);
+            }
+            return 1;
+          } else {
+            let index = otpverifyArray.findIndex((item) => item.Email == email);
+            if (index !== -1) {
+              otpverifyArray.splice(index, 1);
+            }
+            return 0;
+          }
+        } else {
+          return undefined;
+        }
+      } else {
+        return undefined;
+      }
+    } catch (e) {
+      return undefined;
+    }
+  },
+};
+
+const MailToSend = (mailOptions) => {
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(error);
+        reject(error);
+      } else {
+        resolve(info);
+        return true;
+      }
+    });
+  });
+};
+
+
+module.exports = UserVerification;
--- src/routes/v1/signupRoute.js
+++ src/routes/v1/signupRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const authLogic = require('../../logics/v1/authLogic.js');
 const signupLogic = require('../../logics/v1/signupLogic.js');
+const UserVerification = require('../../logics/v1/smtpService.js');
 
 const signupRoute = express.Router();
 
@@ -8,50 +9,64 @@ signupRoute.post('/createuser', async (req, res) => {
     const {email, password} = req.body;
 
     try {
-        const user = await authLogic.checkEmail(email);
-        if(user) {
-            return res.status(401).json({error: 'User Already Exists'})
-        }
-        else {
+        // const user = await authLogic.checkEmail(email);
+        // if(user) {
+        //     return res.status(401).json({error: 'User Already Exists'})
+        // }
+        // else {
             //Hash Password
             let hashedPassword = await authLogic.hashPassword(password);
             //Create User in Database
             const newUser = await signupLogic.addNewUser(email, hashedPassword);
-            try {
-                //generate otp
-                const otp = await signupLogic.generateOTP();
-                //send email with the OTP to the registered Email ID
-                await authLogic.sendOTPEmail(email, otp);
-                //return json object containing _id and generated OTP
-                return res.status(201).json({"OTP" : otp});
-            } catch (err) {
-                console.log("Error Sending Mail", err);
-                return res.status(503).json({ error: 'Mail Service is Down' });
+
+            if(newUser ) {
+                
             }
+            // try {
+            //     //generate otp
+            //     //send email with the OTP to the registered Email ID
+            //     const otp = await UserVerification.sendOtpToUser(email);
+            //     //return json object containing _id and generated OTP
+            //     return res.status(201).json({"OTP" : otp});
+            // } catch (err) {
+            //     console.log("Error Sending Mail", err);
+            //     return res.status(503).json({ error: 'Mail Service is Down' });
+            // }
             const hashPassword = await signupLogic.hashPassword(password);
             }
-        }
+        // }
     catch (e) {
         console.log("Internal Error Occured");
         return res.status(500).json({ error: e.message })
     }
 });
 
-signupRoute.get('/getuserOTP', async (req, res) => {
-    const {userOTP} = req.body;
+signupRoute.get('/validateUser', async (req, res) => {
+    const {email, password, userOTP} = req.body;
     try{
-        const result = await signupLogic.verifyOTP(userOTP);
+        const result = await UserVerification.verifyOTP(email, userOTP);
         if(!result){
             throw "Invalid OTP";
             }
         else{
-            return res.status(200).end()
+            try {
+                const hashedPassword = await signupLogic.hashPassword(password);
+                const newUser = await signupLogic.createUser(email, hashedPassword);
+                console.log(`User Created Successfully with ${email} `);
+                return res.status(200).end()
+            }
+
+           catch (error) {
+                return res.status(401).json({error: error.message});
+           }
         }
     }
     catch(e){
         return res.status(401).json({error: e.message})
     }
 })
+
+
                 
 module.exports = signupRoute;
             
